fbf05112dff4bcb957a66723fb554755a85aea7a
kptst <kptest489@gmail.com>
2025-08-02 21:35:49 +0530

    addint till 2nd aug session

---
+++ Daily sessions/gulvisha.py
@@ -0,0 +1,31 @@
+a=10
+b=12
+c=0
+if a and b and c:
+     print("All number have boolean")
+else:
+    print("at least one number is boolean")
+    
+x=10
+y=-10
+z=0
+if x>0 or y>0:
+    print("either number is greater than 0")
+elif x>0 or y<0:
+    print("any one number is not grater than 0")
+else:
+    print("no number greater than 0")
+    
+    
+p=10
+q=12
+r=12
+
+print(p!=q)
+print(q!=r)
+
+# BMI CALC""
+height=input("tell me H")
+weight=float(input("tell me Weight"))
+BMI= weight/(height/100)**2
+print("Your BMI is : ", BMI)
--- Daily sessions/nael_session.py
+++ Daily sessions/nael_session.py
@@ -13,11 +13,105 @@
 #PATTERNS
 # ACTIVITY-1
 
-print("Half pyramid patterns of (*): ")
-num= int(input("Enter the number of rows: "))
-for i in range(num):
-    for j in range(i+1):
-        print("*", end="")
-    print()
+# print("Half pyramid patterns of (*): ")
+# num= int(input("Enter the number of rows: "))
+# for i in range(num):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print()
+
+# INTRODUCTION TO TURTULE
+
+# import turtle
+# turtle.Screen().bgcolor("red")
+# turtle.Screen().setup(300,400)
+# polygon = turtle.Turtle()
+
+# num_slides =6
+# side_length =70
+# angle =360.0/num_slides
+# for i in range(num_slides):
+#     polygon.forward(side_length)
+#     polygon.right(angle)
     
+# turtle.done()
+
+# SPIRAL PATTERNS
+
+# import turtle
+# my_wn=turtle.Screen()
+# my_wn.bgcolor("blue")
+# my_wn.title("Turtle")
+# my_pen= turtle.Turtle()
+# size=0
+# while True:
+#     for i in range(4):
+#         my_pen.fd(size+1)
+#         my_pen.lef(90)
+#         size=size-5
+#     size = size+1
+
+# --------FUNCTION-------------------
+
+def add(x,y):
+  return x+y
+def subtract(x,y):
+  return x-y
+def multiply(x,y):
+  return x*y
+def divide(x,y):
+  return x/y
+
+print("Please select any one option from below")
+print("A. Add")
+print("B. Subtract")
+print("C. Multiply")
+print("D. Divide")
+
+choice= input("Please enter your choice: ")
+
+num1= int(input("Please enter the first number: "))
+num2= int(input("Please enter the second number: "))
+
+if choice == 'A':
+  print(num1, "+", num2, "=", add(num1, num2))
+elif choice == 'B':
+  print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == 'C':
+  print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == 'D':
+  print(num1, "/", num2, "=", divide(num1, num2))
+else:
+  print("Invalid Input")
+
+# ARGUMENT, FUNTION TYPE, RECURSION, DOCSTRING
+
+# ACTIVITY 1
+ def total_calc(bill_amount,tip_perc):
+   total=bill_amount*(1+0.01*tip_perc)
+   total=round(total,2)
+   print(f"Please pay the bill {total}")
+   
+total_calc(150,20)
+
+# KEYWORDS
+
+a= input("enter words:-")
+for i in a:
+  if(i=='A')
+    print("A is found")
+    break
+  else:
+    print("A is not found")
+    
+    var=10
+    while var>0:
+      var=var-1
+      if var==5:
+        comtinue
+        
+      print("variable value", var)
+
+
+
     
