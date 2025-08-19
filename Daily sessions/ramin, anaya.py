ed724cee63e71f171d165918941422611cafad67
kptst <kptest489@gmail.com>
2025-08-19 20:59:10 +0530

    sessions

---
+++ Daily sessions/ramin, anaya.py
@@ -0,0 +1,59 @@
+# DATA TYPE-ACTIVITY-1
+my_variable = 10
+print(my_variable)
+print(type(my_variable))
+
+my_float_variable= 12.4
+print(my_float_variable)
+print(type(my_float_variable))
+
+my_string="Kamna"
+print(my_string)
+print(type(my_string))
+print(int(my_float_variable)) #TYPE CASTING
+
+
+# Arithmentic-OPERATION-ACTIVITY-3
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print(a//b)
+
+x=2
+y=3
+print(x**y)
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+# STRING-OPERATION-ACTIVITY-3
+first_name="Kamna"
+last_name="Kumari"
+print(first_name+" "+last_name)
+
+# INDEXING OF FIRST NAME
+print(first_name[0])
+print(first_name[1])
+print(first_name[-1])
+print(first_name[0:3])
+print(first_name[0:]) #print(first_name[first_value:end_value]))
+print(first_name[:3])
+
+#Activity-4 - swapping of two variables
+x=input("Enter the value of x:")
+y=input("Enter the value of y:")
+
+temp=x
+x=y
+y=temp
+print("After swapping the value of x is:",x)
+print("After swapping the value of y is:",y)
