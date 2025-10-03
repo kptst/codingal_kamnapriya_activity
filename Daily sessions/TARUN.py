a270289e1750f6074df027921a332d15d5968938
kptst <kptest489@gmail.com>
2025-10-03 18:54:38 +0530

    code1

--- Daily sessions/TARUN.py
+++ Daily sessions/TARUN.py
@@ -281,58 +281,58 @@
 
 # TURTLE-LIBRARY
 
-import turtle
-turtle.Screen().bgcolor("blue")
-turtle.Screen().setup(300,400)
-polygon = turtle.Turtle()
+# import turtle
+# turtle.Screen().bgcolor("blue")
+# turtle.Screen().setup(300,400)
+# polygon = turtle.Turtle()
 
-num_sides =6
-side_length = 70
-angle =360.0/num_sides
-for i in range(num_sides):
-    polygon.forward(side_length)
-    polygon.right(angle)
+# num_sides =6
+# side_length = 70
+# angle =360.0/num_sides
+# for i in range(num_sides):
+#     polygon.forward(side_length)
+#     polygon.right(angle)
     
-turtle.done()
+# turtle.done()
 
 
 # Activity-2 
 
-turtle.Screen().bgcolor("Aqua")
-board = turtle.Turtle()
+# turtle.Screen().bgcolor("Aqua")
+# board = turtle.Turtle()
 
-board.forward(100)
+# board.forward(100)
 
-board.left(120)
-board.forward(100)
-board.left(120)
-board.forward(100)
+# board.left(120)
+# board.forward(100)
+# board.left(120)
+# board.forward(100)
 
-board.penup()
-board.rigth(150)
-board.forward(50)
+# board.penup()
+# board.rigth(150)
+# board.forward(50)
 
-board.pendown()
-board.rigth(90)
-board.forward(100)
+# board.pendown()
+# board.rigth(90)
+# board.forward(100)
 
-board.right(120)
-board.forward(100)
-board.right(120)
-board.forward(100)
+# board.right(120)
+# board.forward(100)
+# board.right(120)
+# board.forward(100)
 
-turtle.done()
+# turtle.done()
 
 
 # Activity-3
-my_wn = turtle.Screen()
-my_wn.bgcolor("orange")
-my_wn.title("Turtle")
-my_pen=turtle.Turtle()
-size=0
-while True:
-    for i in range(4):
-        my_pen.fd(size+1)
-        my_pen.left(90)
-        size=size-5
-    size = size + 1
+# my_wn = turtle.Screen()
+# my_wn.bgcolor("orange")
+# my_wn.title("Turtle")
+# my_pen=turtle.Turtle()
+# size=0
+# while True:
+#     for i in range(4):
+#         my_pen.fd(size+1)
+#         my_pen.left(90)
+#         size=size-5
+#     size = size + 1
